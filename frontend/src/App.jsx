1b9f34b94cbc522019db317071e23ba6d13a1b7c
colinW1206 <wangcolin03@gmail.com>
2026-06-04 18:11:24 +0100

    Frontend: begin contentcontainer & move navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import Navbar from './components/Navbar'
 export default function App() {
   return (
     <>
+      <Navbar />
+      
       <div className="grid grid-rows-3 grid-cols-1 gap-6 p-6">
-        <Navbar />
+        
       </div>
       
     </>
