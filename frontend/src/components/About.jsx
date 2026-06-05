c62884ddbb1de044150e5398a1abd99c77cf4801
colinW1206 <wangcolin03@gmail.com>
2026-06-05 14:12:00 +0100

    Frontend: update about & archive

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,9 +1,22 @@
+import Card from './Card'
+
 export default function About() {
 
     return(
-        <div>
-        
-        </div>
+        <main className="
+            max-w-4xl mx-auto
+            pt-24 px-6 pb-12
+        ">
+            
+            <Card>
+                <h1 className="
+                    text-xl font-bold mb-6
+                ">
+                    About TechTrend Analyser
+                </h1>
+            </Card>
+
+        </main>
     );
 
 }
--- frontend/src/components/Archive.jsx
+++ frontend/src/components/Archive.jsx
@@ -1,7 +1,22 @@
+import Card from './Card'
+
 export default function Archive() {
+    
     return(
-        <div>
-            aaa
-        </div>
+        <main className="
+                    max-w-4xl mx-auto
+                    pt-24 px-6 pb-12
+                ">
+                    
+                    <Card>
+                        <h1 className="
+                            text-xl font-bold mb-6
+                        ">
+                            Archive Place Holder
+                        </h1>
+                    </Card>
+        
+                </main>
     );
+
 }
