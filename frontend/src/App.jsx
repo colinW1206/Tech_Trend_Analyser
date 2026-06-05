86d897b63b766da4f928131e1e003d3d7c143818
colinW1206 <wangcolin03@gmail.com>
2026-06-05 13:16:21 +0100

    Frontend: update footer & begin archive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <div className="min-h-screen bg-slate-400">
       <Navbar />
       <Dashboard />
-       <Footer />
+      <Footer />
     </div>
   )
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,18 @@
 export default function Footer() {
     
     return(
-        <div className='
-            w-full h-20
-            p-6 bg-slate-300
+        <footer className='
+            w-full bg-slate-300
+            border-t-2 border-slate-700
+            py-6 mt-12
         '>
-        </div>
+            <div className='
+                max-w-7xl mx-auto px-6
+                text-center text-xs font-medium
+            '>
+                copy right stuff maybe and links to my linkedin, github etc?
+            </div>
+        </footer>
     );
 
 }
