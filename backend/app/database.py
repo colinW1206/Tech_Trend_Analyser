bfe2a4f4aa65296bb7837367dbdf06f889256005
colinW1206 <wangcolin03@gmail.com>
2026-06-06 13:58:15 +0100

    Backend: update database.py and setup connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,6 +5,16 @@ from sqlmodel import Field, Session, SQLModel, create_engine
 class Story(SQLModel, table=True):
     id: int = Field(primary_key=True)
     title: str
+    url: Optional[str] = None
+    parsed_content: Optional[str] = None # For the clean markdown
 
 class Summary(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    date: str = Field(unique=True, index=True)
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
