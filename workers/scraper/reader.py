d16cb1a6a212c5c591df4699397b242b558a06b6
colinW1206 <wangcolin03@gmail.com>
2026-06-09 13:51:59 +0100

    Scraper: update reader.py

--- workers/scraper/reader.py
+++ workers/scraper/reader.py
@@ -1,15 +1,26 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-HEADERS = {
-    "Authorization": "Bearer [API KEY FROM ENV FILE]"
-}
+load_dotenv()
 
-def extractMarkdown(url):
+JINA_API_KEY = os.getenv("JINA_API_KEY")
 
-    extraction_url = "https://r.jina.ai/https://www.example.com"
+HEADERS = {}
+
+if JINA_API_KEY:
+    HEADERS["Authorisation"] = f"Bearer {JINA_API_KEY}"
+
+def extract_markdown(url):
+    extraction_url = f"https://r.jina.ai/{url}"
+
+    if not url:
+        return None
 
     response = requests.get(extraction_url, headers=HEADERS)
 
-    print(response.text)
-
-    return response.text
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Extraction failed for {url}. Status: {response.status_code}")
+        return None
