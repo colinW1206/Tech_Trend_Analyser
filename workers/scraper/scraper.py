9614c5f21c0bf4b3c3099118c66ac2129e3d5b39
colinW1206 <wangcolin03@gmail.com>
2026-06-30 17:20:43 +0100

    Scraper: refactor to use mongodb

--- workers/scraper/scraper.py
+++ workers/scraper/scraper.py
@@ -1,5 +1,5 @@
 from sqlmodel import Session
-from backend.app.database import engine, Story
+from backend.app.database import stories_collection
 from .reader import extract_markdown
 
 import requests
@@ -9,7 +9,7 @@ top_stories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 response = requests.get(top_stories_url)
 story_ids = response.json()
 
-top_10_ids = story_ids[:3]
+top_10_ids = story_ids[:10]
 
 for story_id in top_10_ids:
     item_url = f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json"
@@ -22,37 +22,34 @@ for story_id in top_10_ids:
     author = story.get('by', 'unknown')
     time_posted = story.get('time', 0)
 
+    existing_story = stories_collection.find_one({"_id": story_id})
 
-    with Session(engine) as session:
+    if not existing_story:
+        original_url = story.get('url')
+        parsed_content = None
 
-        existing_story = session.get(Story, story_id)
+        if original_url:
+            parsed_content = extract_markdown(original_url)
+            time.sleep(0.5)
 
-        if not existing_story:
-            original_url = story.get('url')
-            parsed_content = None
+        story_document = {
+            "_id": story_id,
+            "title": title,
+            "url": url,
+            "score": score,
+            "by": author,
+            "time": time_posted,
+            "parsed_content": parsed_content
+        }
 
-            if original_url:
-                parsed_content = extract_markdown(original_url)
-                time.sleep(0.5)
+        stories_collection.insert_one(story_document)
+        print(f"Added new story: {title}.")
 
-            db_story = Story(
-                id=story_id,
-                title=title,
-                url=url,
-                score=score,
-                by=author,
-                time=time_posted,
-                parsed_content=parsed_content
-            )
-
-            session.add(db_story)
-            session.commit()
-            print(f"Added new story: {title}.")
-
-        else:
-            existing_story.score = score
-            session.add(existing_story)
-            session.commit()
-            print(f"Updated score for: {title}.")
+    else:
+        stories_collection.update_one(
+            {"_id": story_id},
+            {"$set": {"score": score}}
+        )
+        print(f"Updated score for: {title}.")
 
     time.sleep(1)
